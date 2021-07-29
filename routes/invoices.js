9e9845935f58e2d97d467cdee712551031240bc4
Loni <kuangloni@gmail.com>
2021-07-29 14:36:35 -0700

    added use strict

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,5 +1,10 @@
+"use strict"
+
 const express = require("express");
 const router = new express.Router();
 const db = require("../db");
+
+const { NotFoundError } = require("../expressError");
+
 
 module.exports = router;
