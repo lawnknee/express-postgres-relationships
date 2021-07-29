8ebc90455a10ae202428a6cc95c99243b034b055
Loni <kuangloni@gmail.com>
2021-07-29 12:25:37 -0700

    get routes done, post route donee

--- routes/companies.js
+++ routes/companies.js
@@ -2,17 +2,56 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 
-router.get("/", async function (req, res, next) {
-  
-  debugger;
-  const results = await db.query(
-    `SELECT code, name, description
-    FROM companies`);
+const { NotFoundError } = require("../expressError");
 
-  let companies = results.rows;
-  // let companies = "aaaa";
-  
+
+/** GET / => get all companies like:
+ * companies: [{code, name}, ...]} 
+ */
+
+router.get("/", async function (req, res, next) {
+  const results = await db.query(
+    `SELECT code, name
+         FROM companies`);
+  const companies = results.rows;
+
   return res.json({ companies });
 });
+
+
+/** GET /:code => get a single company like:
+ * {company: {code, name, description}} 
+ */
+
+router.get("/:code", async function (req, res, next) {
+  const code = req.params.code;
+
+  const results = await db.query(
+    `SELECT code, name, description
+        FROM companies
+        WHERE code like $1`, [code]
+  );
+  const company = results.rows[0];
+  
+  if (!company) throw new NotFoundError();
+
+  return res.json({ company })
+})
+
+/** POST / => add a new company like:
+ * {company: {code, name, description}}
+*/
+router.post("/", async function(req, res, next) {
+  let {code, name, description} = req.body;
+
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`, [ code, name, description]
+  );
+
+  const company = results.rows[0];
+  return res.status(201).json({ company })
+})
 
 module.exports = router;
